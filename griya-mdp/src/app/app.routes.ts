4658fd3d6c44e3af88092f7c98b1dc423209408f
nicosaputra2327240039-arch <nicosaputra_2327240039@mhs.mdp.ac.id>
2025-11-10 17:01:27 +0700

    griya-mdp

---
+++ griya-mdp/src/app/app.routes.ts
@@ -0,0 +1,32 @@
+import { Routes } from '@angular/router';
+import { Home as    HomeComponent} from './home/home';
+import { Profile } from './profile/profile';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Contact } from './contact/contact';
+
+export const routes: Routes = [
+    //mengatuh halaman utama aplikasi
+    {
+        path : "",
+        component : HomeComponent,
+        title : "Home Page"
+    },
+    {
+        path : "",
+        component : Profile,
+        //title : "Profile Page"
+    },
+    {
+        path : "",
+        component : Login,
+    },
+    {
+        path : "",
+        component : Register,
+    },
+    {
+        path : "",
+        component : Contact,
+    },
+];
